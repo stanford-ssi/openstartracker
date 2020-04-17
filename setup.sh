7b003e33771ff938bb46e81f51f2118087472722
Rodrigo Castellon <rjcaste@stanford.edu>
2020-04-16 21:43:53 -0400

    fixed typo

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ docker build -t startracker1 .
 # directory of this setup.sh script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-alias dstart='docker run startracker1 --mount source:'$DIR',target=/'
+alias dstart='docker run -it -v '$DIR'/:/home startracker1'
